e1d0bda1c163d8590f8cca00e1c6f723ee3528e0
nisha <nisharp07@gmail.com>
2024-04-30 16:53:07 +0530

    payment type

---
+++ src/main/java/com/hotel/controller/PaymentController.java
@@ -0,0 +1,10 @@
+package com.hotel.controller;
+
+public class PaymentController {
+
+    private String paymentType;
+
+    public PaymentController(String paymentType) {
+        this.paymentType = paymentType;
+    }
+}
